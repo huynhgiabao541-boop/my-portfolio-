424ef967e086494da47b60e161738828043afdd6
Gia Bảo Huỳnh <huynhgiabao541@gmail.com>
2026-09-12 10:27:07 +0700

    update tsx

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,14 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import path from 'path'
 
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      // Cấu hình alias @ trỏ thẳng vào thư mục src
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
+
 });
